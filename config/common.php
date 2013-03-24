860c453aca0880bc98bfc99393e925b6bf3a3d08
王秋石 <qiushi@reco.cn>
2013-03-24 15:41:56 +0800

    hehe

--- config/common.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config['version'] = '1.0';
-$config['name'] = 'RSF';
-$config['company'] = 'RECO';
-$config['source'] = 'http://source.qiushi.dev.reco.cn';
-$config['static'] = 'http://static.qiushi.dev.reco.cn';
